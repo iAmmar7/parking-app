6652ca9ce8549b38943150d52804d3266cf5d6e3
Ammar Ansari <ammar.ansari@ext.careem.com>
2022-05-21 21:34:39 +0500

    europe region set for functions

--- functions/index.js
+++ functions/index.js
@@ -5,12 +5,12 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-exports.testFunction = functions.https.onCall((data, context) => {
+exports.testFunction = functions.region('europe-west3').https.onCall((data, context) => {
   functions.logger.info('Test function logs!');
   return 'Hello from Firebase test function!';
 });
 
-exports.testAuthFunction = functions.https.onCall(async (data, context) => {
+exports.testAuthFunction = functions.region('europe-west3').https.onCall(async (data, context) => {
   if (!context.auth) {
     throw new functions.https.HttpsError('unauthenticated', 'You are not authorized!');
   }
